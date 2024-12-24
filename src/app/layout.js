9b70d58b63c3752d17f523b960ae385b312818f3
pula-ashok <ashokpula0307@gmail.com>
2024-12-24 11:15:02 +0530

    installed clerk nextjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import { ThemeProvider } from "next-themes";
 import Theme from "./components/Theme";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+  <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -33,5 +35,6 @@ export default function RootLayout({ children }) {
         </ThemeProvider>
       </body>
     </html>
+  </ClerkProvider>
   );
 }
