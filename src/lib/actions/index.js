633688e0c903a790e9c3e915881c83a02e4606f7
pula-ashok <ashokpula0307@gmail.com>
2024-12-26 00:06:22 +0530

    fix for username

--- src/lib/actions/index.js
+++ src/lib/actions/index.js
@@ -18,7 +18,7 @@ export const createOrUpdateUser = async (
           lastName: last_name,
           profilePicture: image_url,
           email: email_addresses[0]?.email_address,
-          username,
+          userName: username,
         },
       },
       { new: true, upsert: true }
