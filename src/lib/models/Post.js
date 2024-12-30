476993d45b8fa895a0c4bae4627a8cf8d4572012
pula-ashok <ashokpula0307@gmail.com>
2024-12-30 12:01:36 +0530

    slug type updated

--- src/lib/models/Post.js
+++ src/lib/models/Post.js
@@ -6,7 +6,7 @@ const postSchema=new mongoose.Schema({
     content:{type:String,required:true}, 
     category:{type:String,default:"uncategorized"},
     image:{type:String,default:"https://cdn.britannica.com/48/252748-050-C514EFDB/Virat-Kohli-India-celebrates-50th-century-Cricket-November-15-2023.jpg"},
-    slug:{type:Date,required:true,unique:true},
+    slug:{type:String,required:true,unique:true},
 },{timestamps:true})
 
 const Post=mongoose.models.Post || mongoose.model("Post",postSchema)
